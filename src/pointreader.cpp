1b35ee8b61a9bd0ad9a2ca59b7b3ea52540ce449
PEIOPA <peio.palacios@alumni.mondragon.edu>
2025-01-23 12:29:17 +0100

    Update pointreader.cpp

--- src/pointreader.cpp
+++ src/pointreader.cpp
@@ -1,36 +1,34 @@
-
-#define _CRT_SECURE_NO_WARNINGS
-#include "pointreader.h"
+#include <csv.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include "point.h"
+#include "pointreader.h"
+#include "angularvelocitycalculator.h"
 
-int readPoints(const char* file, point_t** points) {
-    FILE* fp = fopen(file, "r");
-    if (!fp) return -2; // Archivo no encontrado
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		printf("Program to compute maximum angular velocity from a txt file of comma separated values produced by Tracker\n");
+		printf("Usage: %s <filename>\n", argv[0]);
+		return -1;
+	}
+	struct point_t* points;
 
-    int capacity = 10;
-    *points = (point_t*)malloc(capacity * sizeof(point_t));
-    if (!*points) return -1; // Error al asignar memoria
+	int numberOfPoints = readPoints(argv[1], &points);
 
-    int count = 0;
-    while (!feof(fp)) {
-        if (count >= capacity) {
-            capacity *= 2;
-            point_t* temp = (point_t*)realloc(*points, capacity * sizeof(point_t));
-            if (!temp) {
-                free(*points);
-                return -1; // Error de reasignación
-            }
-            *points = temp;
-        }
-        // Suponiendo que el archivo tiene columnas: x, y, t (tres valores por línea)
-        if (fscanf_s(fp, "%f,%f,%f\n", &(*points)[count].x, &(*points)[count].y, &(*points)[count].t) == 3) {
-            count++;
-        }
-        else {
-            break; // Error de formato
-        }
-    }
-    fclose(fp);
-    return count;
+	if (numberOfPoints < 0)
+	{
+		printf("Error reading file\n");
+		return -1;
+	}
+	float maximumAngularVelocity = computeMaximumAngularVelocity(points, numberOfPoints);
+
+	//Como el video esta grabado en camara lenta, 8 veces mas lento delo normal, multiplicaremos el resultado por 8
+	//Aparte, vamos a pasar de radian/segundo a rpm, para ello, lo multiplicaremos por 60/2pi
+	maximumAngularVelocity = maximumAngularVelocity * 8 * (60 / (2 * 3.14));
+
+	printf("Maximum angular velocity: %f rpm\n", maximumAngularVelocity);
+
+	freePoints(&points);
+	return 0;
 }
